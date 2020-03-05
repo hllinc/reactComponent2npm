e6eef9bfce147f759b6822e6dafbdce9faea3c80
hllinc <hllinc@163.com>
2020-03-05 22:22:48 +0800

    组件通过props传参。

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import * as styles from './index.css';
-class ReactDemo extends React.Component{
-  render () {
-    return <div className={styles.wrapper}>hello world</div>
+import PropTypes from 'prop-types'
+
+class ReactDemo extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.props = {
+      type: PropTypes.string,
+      value: PropTypes.string
+    };
+  }
+
+  render() {
+    const {
+      value,
+      type
+    } = this.props;
+    return <div className={styles.timChatWidget}>
+      <p>咚咚运维</p>
+      <a href={value}>点我</a>
+    </div>
   }
 }
+
 export default ReactDemo;
